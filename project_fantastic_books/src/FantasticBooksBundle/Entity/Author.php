06f12857021a858f3a6115b284cdc65df1ad880d
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-02-23 10:55:50 +0100

    CRUD: Author

---
+++ project_fantastic_books/src/FantasticBooksBundle/Entity/Author.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace FantasticBooksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Author
+ *
+ * @ORM\Table(name="author")
+ * @ORM\Entity(repositoryClass="FantasticBooksBundle\Repository\AuthorRepository")
+ */
+class Author
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=100)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=100)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alias", type="string", length=255, nullable=true)
+     */
+    private $alias;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="linkToHomePage", type="string", length=255, nullable=true)
+     */
+    private $linkToHomePage;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return Author
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     * @return Author
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set alias
+     *
+     * @param string $alias
+     * @return Author
+     */
+    public function setAlias($alias)
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    /**
+     * Get alias
+     *
+     * @return string 
+     */
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    /**
+     * Set linkToHomePage
+     *
+     * @param string $linkToHomePage
+     * @return Author
+     */
+    public function setLinkToHomePage($linkToHomePage)
+    {
+        $this->linkToHomePage = $linkToHomePage;
+
+        return $this;
+    }
+
+    /**
+     * Get linkToHomePage
+     *
+     * @return string 
+     */
+    public function getLinkToHomePage()
+    {
+        return $this->linkToHomePage;
+    }
+}
