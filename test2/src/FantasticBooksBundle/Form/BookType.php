cf1b02c7fa24691cad9e288c304bf9cf7fc1ef59
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-02-24 10:39:25 +0100

    CRUD: Book

---
+++ test2/src/FantasticBooksBundle/Form/BookType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace FantasticBooksBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titlePolish')->add('titleEnglish')->add('titleOriginal')->add('series')->add('volume')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FantasticBooksBundle\Entity\Book'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'fantasticbooksbundle_book';
+    }
+
+
+}
