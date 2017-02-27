9f4bcd4c69c4b2e733faa0c4a1023195a49a9296
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-02-27 23:53:24 +0100

    searchCharacterAction: search by character name

--- project_fantastic_books_v2/src/FantasticBooksBundle/Controller/SearchController.php
+++ project_fantastic_books_v2/src/FantasticBooksBundle/Controller/SearchController.php
@@ -4,14 +4,39 @@ namespace FantasticBooksBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends Controller
 {
+
     /**
      * @Route("/searchCharacter")
      */
-    public function searchCharacterAction()
+    public function searchCharacterAction(Request $request)
     {
+        if ($request->isMethod(Request::METHOD_POST)) {
+
+            $name = $request->get('name');
+
+
+            $em = $this->getDoctrine()->getManager();
+
+
+            $query = $em->createQuery(
+                'SELECT p
+                FROM FantasticBooksBundle:CharacterFromBook p WHERE
+                p.name = :name
+                ORDER BY p.name DESC'
+            )->setParameter('name', $name);
+
+            $characterFromBooks = $query->getResult();
+
+            return $this->render('FantasticBooksBundle:Search:show_character.html.twig', array(
+                'characterFromBooks' => $characterFromBooks,
+            ));
+
+        }
+
         return $this->render('FantasticBooksBundle:Search:search_character.html.twig',  [
             'form_address' => $this->generateUrl('fantasticbooks_search_searchcharacter')
         ]);
@@ -20,8 +45,9 @@ class SearchController extends Controller
     /**
      * @Route("/showCharacter")
      */
-    public function showCharacterAction()
+    public function showCharacterAction($name)
     {
+
         return $this->render('FantasticBooksBundle:Search:show_character.html.twig', array(
             // ...
         ));
