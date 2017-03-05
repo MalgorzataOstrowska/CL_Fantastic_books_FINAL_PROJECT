28a448a63cbc683089c06216c251bf4a1d3fdc01
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-03-06 00:37:13 +0100

    ShowCharacterController: indexAction

--- project_fantastic_books_v2/src/FantasticBooksBundle/Controller/ShowCharacterController.php
+++ project_fantastic_books_v2/src/FantasticBooksBundle/Controller/ShowCharacterController.php
@@ -4,16 +4,32 @@ namespace FantasticBooksBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ShowCharacterController extends Controller
 {
     /**
      * @Route("character/index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $em    = $this->get('doctrine.orm.entity_manager');
+        $dql   = "SELECT a 
+                  FROM FantasticBooksBundle:CharacterFromBook a
+                  ORDER BY a.name ASC
+                  ";
+        $query = $em->createQuery($dql);
+
+        $paginator  = $this->get('knp_paginator');
+        $characterFromBooks = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
+        // parameters to template
         return $this->render('FantasticBooksBundle:ShowCharacter:index.html.twig', array(
-            // ...
+            'characterFromBooks' => $characterFromBooks
         ));
     }
 
